455f00fe5e9ff483205c545e55274a88a55660b2
prathmeshpote99 <poteprathmesh@gmail.com>
2024-04-02 19:05:38 +0530

    feat: add profile svg

--- resources/js/Pages/NewShowProfile.jsx
+++ resources/js/Pages/NewShowProfile.jsx
@@ -66,7 +66,27 @@ const NewShowProfile = () => {
                                     novalidate=""
                                     // onSubmit={handleSubmit}
                                 >
-                                    <div className="row gy-3 gy-md-4 overflow-hidden">
+                                    <div className="d-flex justify-content-center align-items-center mb-3">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            width="50"
+                                            height="50"
+                                            fill="currentColor"
+                                            class="bi bi-person-circle"
+                                            viewBox="0 0 16 16"
+                                        >
+                                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                                            <path
+                                                fill-rule="evenodd"
+                                                d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
+                                            />
+                                        </svg>
+                                    </div>
+
+                                    <div
+                                        className="row gy-3 gy-md-4 overflow-hidden"
+                                        style={{ marginLeft: "0%" }}
+                                    >
                                         <div className="col-6">
                                             <label
                                                 for="validationCustom01"
